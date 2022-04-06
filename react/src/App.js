f35ea4c9156222860ceca89fe36142b75959842d
jain-naveen-au25 <87427392+jain-naveen-au25@users.noreply.github.com>
2022-04-06 12:11:09 +0530

    Create App.js

---
+++ react/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Movies from './components/movies'
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Let's Watch Movies</h1>
+      <Movies></Movies>
+      
+    </div>
+  );
+}

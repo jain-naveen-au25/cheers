717028ec1972123e88201bd4fe4f208b30ec8a06
jain-naveen-au25 <87427392+jain-naveen-au25@users.noreply.github.com>
2022-04-06 12:12:53 +0530

    Create movies.js

---
+++ react/src/movies.js
@@ -0,0 +1,77 @@
+let movies=[
+  {
+      title: "The Shawshank Redemption",
+      rank: 1,
+      id: "tt0111161",
+      ratings:5,
+      release:"04-03-2021"
+
+
+
+  },
+  {
+      title: "The Godfather",
+      rank: 2,
+      id: "tt0068646",
+      ratings:5,
+      release:"04-03-2021"
+  },
+  {
+      title: "The Godfather: Part II",
+      rank: 3,
+      id: "tt0071562",
+      ratings:5,
+      release:"04-03-2021"
+  },
+  {
+      title: "Pulp Fiction",
+      rank: 4,
+      id: "tt0110912",
+      ratings:5,
+      release:"04-03-2021"
+  },
+  {
+      title: "The Good, the Bad and the Ugly",
+      rank: 5,
+      id: "tt0060196",
+      ratings:5,
+      release:"04-03-2021"
+  },
+  {
+      title: "The Dark Knight",
+      rank: "6",
+      id: "tt0468569",
+      ratings:5,
+      release:"04-03-2021"
+  },
+  {
+      title: "12 Angry Men",
+      rank: 7,
+      id: "tt0050083",
+      ratings:5,
+      release:"04-03-2021"
+  },
+  {
+      title: "Schindler's List",
+      rank:8,
+      id: "tt0108052",
+      ratings:5,
+      release:"04-03-2021"
+  },
+  {
+      title: "The Lord of the Rings: The Return of the King",
+      rank: 9,
+      id: "tt0167260",
+      ratings:5,
+      release:"04-03-2021"
+  },
+  {
+      title: "Fight Club",
+      rank: 10,
+      id: "tt0137523",
+      ratings:5,
+      release:"04-03-2021"
+  }]
+  export default movies;
+  
+  
